8ac992e77f4544ce7db4338eebaa4e3dfeeb86a7
tracer-ics2021 <tracer@njuics.org>
2021-12-15 22:06:49 +0800

    >  compile 201220000

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -1,7 +1,7 @@
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
 LLVM_VERSION=$(shell  llvm-config --version 2>/dev/null || echo "Not found")
-ifeq ($(LLVM_VERSION),"13.0.0")
+ifeq ($(LLVM_VERSION),'13.0.0')
         LLVM_CONFIG = llvm-config
 else
         LLVM_CONFIG = llvm-config-11
