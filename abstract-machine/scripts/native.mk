13e89b5211a470f48109528850f8a8ac8bedb9e5
OkitaSan <tssjy999@163.com>
2021-12-15 18:43:37 +0800

    abstract-machine ics2021 initialized
    abstract-machine f9b9b390fb673cee2d733106db077efda65bb304 fix dead recursion when __NATIVE_USE_KLIB__ is defined

---
+++ abstract-machine/scripts/native.mk
@@ -0,0 +1,25 @@
+AM_SRCS := native/trm.c \
+           native/ioe.c \
+           native/cte.c \
+           native/trap.S \
+           native/vme.c \
+           native/mpe.c \
+           native/platform.c \
+           native/ioe/input.c \
+           native/ioe/timer.c \
+           native/ioe/gpu.c \
+           native/ioe/audio.c \
+           native/ioe/disk.c \
+
+CFLAGS  += -fpie
+ASFLAGS += -fpie -pie
+
+image:
+	@echo + LD "->" $(IMAGE_REL)
+	@g++ -pie -o $(IMAGE) -Wl,--whole-archive $(LINKAGE) -Wl,-no-whole-archive -lSDL2 -ldl
+
+run: image
+	$(IMAGE)
+
+gdb: image
+	gdb -ex "handle SIGUSR1 SIGUSR2 SIGSEGV noprint nostop" $(IMAGE)
