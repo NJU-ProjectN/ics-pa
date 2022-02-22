6394497a82e309164a2dc69158890c178ffa68b1
tracer-ics2021 <tracer@njuics.org>
2022-02-22 21:51:27 +0800

    >  compile 201220000    Static hostname: LOS          Icon name: computer-vm            Chassis: vm         Machine ID: 5a180b0bcff34b7a92e6bdaf4eb03a53            Boot ID: ffc7634f296f42e081bb63e18c1a70c7     Virtualization: kvm   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.4.0-96-generic       Architecture: x86-64  21:51:27 up 23 days, 46 min,  0 users,  load average: 0.13, 0.08, 0.08

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -1,5 +1,5 @@
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
-LIBS += $(shell llvm-config --libs)
+CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIE
+LIBS += $(shell llvm-config-11 --libs)
 endif
