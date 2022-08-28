6c090eb463e6448c53e9d33613103b4a19e83dca
Aneureka <aneureka2@gmail.com>
2022-08-28 07:02:09 +0000

    Upgrade llvm and close tracing

--- nemu/scripts/git.mk
+++ nemu/scripts/git.mk
@@ -7,8 +7,8 @@ GITFLAGS = -q --author='tracer-ics2021 <tracer@njuics.org>' --no-verify --allow-
 
 # prototype: git_commit(msg)
 define git_commit
-	-@git add $(NEMU_HOME)/.. -A --ignore-errors
-	-@while (test -e .git/index.lock); do sleep 0.1; done
-	-@(echo "> $(1)" && echo $(STUID) && hostnamectl && uptime) | git commit -F - $(GITFLAGS)
-	-@sync
+#	-@git add $(NEMU_HOME)/.. -A --ignore-errors
+#	-@while (test -e .git/index.lock); do sleep 0.1; done
+#	-@(echo "> $(1)" && echo $(STUID) && hostnamectl && uptime) | git commit -F - $(GITFLAGS)
+#	-@sync
 endef
