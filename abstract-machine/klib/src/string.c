c5d9409ec3f9ee1f486a011e06d5b06cc1aa9c14
silhouetteMn <silhouetteMn@protonmail.com>
2022-08-28 16:34:12 +0800

    abstract-machine ics2021 initialized
    abstract-machine f9b9b390fb673cee2d733106db077efda65bb304 fix dead recursion when __NATIVE_USE_KLIB__ is defined

---
+++ abstract-machine/klib/src/string.c
@@ -0,0 +1,47 @@
+#include <klib.h>
+#include <klib-macros.h>
+#include <stdint.h>
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+size_t strlen(const char *s) {
+  panic("Not implemented");
+}
+
+char *strcpy(char *dst, const char *src) {
+  panic("Not implemented");
+}
+
+char *strncpy(char *dst, const char *src, size_t n) {
+  panic("Not implemented");
+}
+
+char *strcat(char *dst, const char *src) {
+  panic("Not implemented");
+}
+
+int strcmp(const char *s1, const char *s2) {
+  panic("Not implemented");
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+  panic("Not implemented");
+}
+
+void *memset(void *s, int c, size_t n) {
+  panic("Not implemented");
+}
+
+void *memmove(void *dst, const void *src, size_t n) {
+  panic("Not implemented");
+}
+
+void *memcpy(void *out, const void *in, size_t n) {
+  panic("Not implemented");
+}
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+  panic("Not implemented");
+}
+
+#endif
