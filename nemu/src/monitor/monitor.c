e0798c5e6576f768a97914b9d20c2c570f1b5ffb
tracer-ics2023 <tracer@njuics.org>
2024-01-06 18:43:53 +0800

    >  compile NEMU 20031023 wcy Linux wcy 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:53 up 3 days, 20:53,  1 user,  load average: 0.30, 0.66, 2.21

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -33,7 +33,7 @@ static void welcome() {
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
   Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(0);
+  //assert(0);
 }
 
 #ifndef CONFIG_TARGET_AM
