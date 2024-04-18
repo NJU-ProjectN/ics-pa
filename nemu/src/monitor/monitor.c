eb3cc380f5cf257fd74a8a26db9e8ee88e329ea2
tracer-ics2022 <tracer@njuics.org>
2024-04-18 18:53:40 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:53:40 up  1:12,  1 user,  load average: 2.06, 1.76, 1.59

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -29,12 +29,12 @@ static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
-        "If it is not necessary, you can disable it in menuconfig"));
+       "If it is not necessary, you can disable it in menuconfig"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
-  //Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(0);
+  Log("Exercise: Please remove me in the source code and compile NEMU again.");
+  //assert(0);
 }
 
 #ifndef CONFIG_TARGET_AM
