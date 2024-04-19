0e7d3cd9f2ec0a245c6eac1743281cf84ef80254
tracer-ics2022 <tracer@njuics.org>
2024-04-19 13:04:28 +0800

    >  compile NEMU 23030112 puji Linux pu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:04:28 up 8 min,  1 user,  load average: 1.21, 1.03, 0.56

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -20,5 +20,5 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
-  return good;
+  return !good;
 }
