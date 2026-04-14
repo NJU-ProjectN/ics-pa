9216e453c6f9fb95a2cbf4d46d7d5aa2fde4b6d7
tracer-ics2017 <tracer@njuics.org>
2026-04-14 11:53:48 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-14-generic #14~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jan 15 15:52:10 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:53:48 up  1:54,  3 users,  load average: 0.07, 0.03, 0.00 55e3a23a4c966bf223324b9273a10c99c28041da

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -16,24 +16,23 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
 typedef struct {
   union{
-    union{
-      struct {
+    struct{
+     union{
         uint32_t _32;
         uint16_t _16;
         uint8_t _8[2];
-      } gpr[8];
-    }
-    struct {
-      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+      }gpr[8];
     };
-  };
+
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+    struct {
+      rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };
+  };
   vaddr_t eip;
 
 } CPU_state;
