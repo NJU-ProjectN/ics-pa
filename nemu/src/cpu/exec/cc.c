83d218623dceb99e3718ade9121d79472aac5a49
tracer-ics2017 <tracer@njuics.org>
2026-05-19 19:49:23 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:49:23 up  3:25,  3 users,  load average: 0.37, 0.09, 0.03 3aec2a7ca8ea2aeb4b5884bce627b9069919f79

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -10,76 +10,43 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_S, CC_NS, CC_P,  CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
-
+  rtlreg_t t0 = 0, t1 = 0;
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-      rtl_get_OF(dest); 
-      break;
-    case CC_NO:
+    case CC_O: // 对应 O 和 NO
       rtl_get_OF(dest);
-      rtl_xori(dest, dest, 0x1);
       break;
-    case CC_B:
-      rtl_get_CF(dest); 
-      break;
-    case CC_NB:
+    case CC_B: // 对应 B 和 NB (jc / jnc)
       rtl_get_CF(dest);
-      rtl_xori(dest, dest, 0x1);
       break;
-    case CC_E:
-      rtl_get_ZF(dest); 
-      break;
-    case CC_NE:
+    case CC_E: // 对应 E 和 NE (je / jne)
       rtl_get_ZF(dest);
-      rtl_xori(dest, dest, 0x1);
       break;
-    case CC_BE:
-      rtl_get_CF(dest);
+    case CC_BE: // 对应 BE 和 NBE (jbe / jnbe)
+      // 条件是：CF == 1 或 ZF == 1
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      rtl_or(dest, &t0, &t1);
       break;
-    case CC_NBE:
-      rtl_get_CF(dest);
-      rtl_xori(dest, dest, 0x1);
-      break;
-    case CC_S:
-      rtl_get_SF(dest); 
-      break;
-    case CC_NS:
+    case CC_S: // 对应 S 和 NS
       rtl_get_SF(dest);
-      rtl_xori(dest, dest, 0x1);
       break;
+    case CC_L: // 对应 L 和 NL (jl / jnl)
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(dest, &t0, &t1);
+      break;
+    case CC_LE: // 对应 LE 和 NLE (jle / jnle)
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(&t0, &t0, &t1); // t0 = SF ^ OF
+      rtl_get_ZF(&t1);        // t1 = ZF
+      rtl_or(dest, &t0, &t1); // dest = (SF ^ OF) | ZF
+      break;
+      
     case CC_P: panic("n86 does not have PF");
-    case CC_NP: panic("n86 does not have PF");
-    case CC_L:
-      rtl_get_SF(dest);
-      rtl_get_OF(dest);
-      break;
-    case CC_NL:
-      rtl_get_SF(dest);
-      rtl_get_OF(dest);
-      rtl_xor(dest, dest, &t0);
-      rtl_xori(dest, dest, 0x1);
-      break;
-    case CC_LE:
-      rtl_get_SF(dest);
-      rtlreg_t sf, of;
-      rtl_get_SF(&sf);
-      rtl_get_OF(&of);
-      rtl_xor(&t0, &sf, &of);
-      rtl_or(dest, dest, &t0);
-      rtl_get_ZF(dest);
-      break;
-    case CC_NLE:
-      rtl_get_SF(dest);
-      rtl_get_OF(dest);
-      rtl_xor(&t0, &sf, &of);
-      rtl_or(dest, dest, &t0);
-      rtl_get_ZF(dest);
-      rtl_xori(dest, dest, 0x1);
-      break;
     default: panic("should not reach here");
-
   }
 
   if (invert) {
