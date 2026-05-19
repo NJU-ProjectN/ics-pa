1f7b586bd8afa5c52395a25b96d51cbee1c71edc
tracer-ics2017 <tracer@njuics.org>
2026-05-19 18:14:31 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:14:31 up  1:53,  2 users,  load average: 0.00, 0.00, 0.00 b898b052f1784d617f6ec021ae40742b410475c1

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,11 +6,18 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  if (id_dest->type == OP_TYPE_MEM) {
-    rtl_lm(&id_dest->val, &id_dest->addr, id_dest->width);
+  if (id_src->type == OP_TYPE_MEM) {
+    rtl_lm(&id_src->val, &id_src->addr, id_src->width);
+    rtl_push(&id_src->val);
   }
-
-  rtl_push(&id_dest->val);
+  else if (id_dest->type == OP_TYPE_MEM) {
+    rtl_lm(&id_dest->val, &id_dest->addr, id_dest->width);
+    rtl_push(&id_dest->val);
+  }
+  else {
+    rtlreg_t *val = (id_src->type == OP_TYPE_REG || id_src->type == OP_TYPE_IMM) ? &id_src->val : &id_dest->val;
+    rtl_push(val);
+  }
 
   
   print_asm_template1(push);
