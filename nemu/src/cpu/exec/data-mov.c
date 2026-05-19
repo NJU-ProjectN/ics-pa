99cd9aebcef7549092b5fbbf0fc17816c6e80c10
tracer-ics2017 <tracer@njuics.org>
2026-05-19 17:47:48 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:48 up  1:26,  2 users,  load average: 0.22, 0.09, 0.02 f90e9a74a5f5deed8156ade5fa478855c02a8fd9

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -137,7 +137,6 @@ make_EHelper(movzx) {
 }
 
 make_EHelper(lea) {
-  rtl_li(&t2, id_src->addr);
-  operand_write(id_dest, &t2);
+  operand_write(id_dest, &id_src->addr);
   print_asm_template2(lea);
 }
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -8,6 +8,7 @@ make_EHelper(ret);
 make_EHelper(sub);
 make_EHelper(xor);
 make_EHelper(cmp);
+make_EHelper(lea);
 
 typedef struct {
   DHelper decode;
@@ -115,7 +116,7 @@ opcode_entry opcode_table [512] = {
   /* 0x80 */	IDEXW(I2E, gp1, 1), IDEX(I2E, gp1), EMPTY, IDEX(SI2E, gp1),
   /* 0x84 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x88 */	IDEXW(mov_G2E, mov, 1), IDEX(mov_G2E, mov), IDEXW(mov_E2G, mov, 1), IDEX(mov_E2G, mov),
-  /* 0x8c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x8c */	EMPTY, IDEX(E2G, lea), EMPTY, EMPTY,
   /* 0x90 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x94 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x98 */	EMPTY, EMPTY, EMPTY, EMPTY,
