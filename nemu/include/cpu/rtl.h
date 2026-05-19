c229dfa17c8899d9f6d7a158e725e566098b5356
tracer-ics2017 <tracer@njuics.org>
2026-05-19 17:30:50 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:30:50 up  1:09,  2 users,  load average: 0.00, 0.00, 0.00 b239ab6b329599b66af74076acc73e551733b64a

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -162,8 +162,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  rtl_li(&t0, 4);
-  rtl_sub(&cpu.esp, &cpu.esp, &t0);
+  cpu.esp -= 4;
   rtl_sm(&cpu.esp, 4, src1);
 }
 
@@ -171,8 +170,7 @@ static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
   rtl_lm(dest, &cpu.esp, 4);
-  rtl_li(&t0, 4);
-  rtl_add(&cpu.esp, &cpu.esp, &t0);
+  cpu.esp += 4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
