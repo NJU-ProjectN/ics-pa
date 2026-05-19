8d16f91a08d8c1736d61da325e5a482505cb8fae
tracer-ics2017 <tracer@njuics.org>
2026-05-19 17:23:57 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:57 up  1:02,  2 users,  load average: 0.00, 0.00, 0.00 a3db2be389ec80b7448c581deac4d195444f5a69

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,10 +26,11 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_push(&decoding.seq_eip);
+  decoding.jmp_eip = decoding.seq_eip + id_dest->val;
   decoding.is_jmp = 1;
 
   print_asm("call %x", decoding.jmp_eip);
-}
+} 
 
 make_EHelper(ret) {
   rtl_pop(&t0);
