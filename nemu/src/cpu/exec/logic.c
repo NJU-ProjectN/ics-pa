a48fa2f4ecd8c186fac11d35feb0743041cb45c4
tracer-ics2017 <tracer@njuics.org>
2026-05-19 18:43:47 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:43:47 up  2:19,  3 users,  load average: 0.11, 0.04, 0.01 ab1a5388d77bd27f21626a5ec0af0c82e698bd66

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -77,11 +77,10 @@ make_EHelper(shr) {
 }
 
 make_EHelper(setcc) {
-  uint8_t subcode = decoding.opcode & 0xf;
-  rtl_setcc(&t2, subcode);
+  rtl_get_ZF(&t2);
   operand_write(id_dest, &t2);
 
-  print_asm("set%s %s", get_cc_name(subcode), id_dest->str);
+  print_asm_template1(setcc);
 }
 
 make_EHelper(not) {
