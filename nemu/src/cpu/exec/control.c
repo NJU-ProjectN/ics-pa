625593db0106b2573c1dd6480339d41ed299efec
tracer-ics2017 <tracer@njuics.org>
2026-05-19 09:09:20 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-20-generic #20~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 19 01:28:37 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:09:20 up 27 min,  2 users,  load average: 0.11, 0.07, 0.05 59ab6f8cc82ad6852be9d43944afde6dabf9d370

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -45,3 +45,53 @@ make_EHelper(call_rm) {
 
   print_asm("call *%s", id_dest->str);
 }
+
+make_EHelper(ret_imm) {
+  rtl_pop(&decoding.jmp_eip);
+  rtl_addi(&cpu.esp, &cpu.esp, id_dest->val);
+  decoding.is_jmp = 1;
+
+  print_asm("ret %d", id_dest->val);
+}
+
+make_EHelper(xor_rm2r) {
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  print_asm("xor %s, %s", id_dest->str, id_src->str);
+}
+
+make_EHelper(xor_i2rm) {
+  rtl_xor(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  print_asm("xor %s, %s", id_dest->str, id_src->str);
+}
+
+make_EHelper(push) {
+  rtl_push(&id_dest->val);
+
+  print_asm("push %s", id_dest->str);
+}
+
+make_EHelper(pop) {
+  rtl_pop(&t2);
+  operand_write(id_dest, &t2);
+
+  print_asm("pop %s", id_dest->str);
+}
+
+make_EHelper(sub_rm2r) {
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  print_asm("sub %s, %s", id_dest->str, id_src->str);
+}
+
+make_EHelper(sub_i2rm) {
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+
+  print_asm("sub %s, %s", id_dest->str, id_src->str);
+}
+
