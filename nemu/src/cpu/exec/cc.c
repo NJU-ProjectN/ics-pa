fe7cc874f2977a026ebe57f5c9afb6b6ad1320a0
tracer-ics2017 <tracer@njuics.org>
2026-05-19 19:08:09 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:08:09 up  2:43,  3 users,  load average: 0.01, 0.04, 0.00 ba4b3185b03e5c361e94703f5c6b54083a821a76

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,13 +15,33 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+      rtl_get_OF(dest); 
+      break;
     case CC_B:
+      rtl_get_CF(dest); 
+      break;
     case CC_E:
+      rtl_get_ZF(dest); 
+      break;
     case CC_BE:
+      rtl_get_CF(dest);
+      break;
     case CC_S:
+      rtl_get_SF(dest); 
+      break;
     case CC_L:
+      rtl_get_SF(dest);
+      rtl_get_OF(dest);
+      break;
     case CC_LE:
-      TODO();
+      rtl_get_SF(dest);
+      rtlreg_t sf, of;
+      rtl_get_SF(&sf);
+      rtl_get_OF(&of);
+      rtl_xor(&t0, &sf, &of);
+      rtl_or(dest, dest, &t0);
+      rtl_get_ZF(dest);
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
