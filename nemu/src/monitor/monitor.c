ceed35916daa8a4417d7c16a4a49b662f6e34967
tracer-ics2017 <tracer@njuics.org>
2026-05-19 20:07:08 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:08 up  3:42,  3 users,  load average: 0.07, 0.10, 0.04 49374fdc696e21c4a4d32cdced32f0678be39829

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,7 +82,7 @@ static inline void load_img() {
 static inline void restart() {
   /* Set the initial instruction pointer. */
   cpu.eip = ENTRY_START;
-
+  cpu.esp = 0x7c00;
 #ifdef DIFF_TEST
   init_qemu_reg();
 #endif
