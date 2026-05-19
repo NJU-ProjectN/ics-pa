24f972618c33c3d0911008994bb665764f977a7f
tracer-ics2017 <tracer@njuics.org>
2026-05-19 18:47:30 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:30 up  2:23,  3 users,  load average: 0.07, 0.03, 0.00 6c81cac1117285b28e147c3548b80b4a6ec18ca2

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -229,6 +229,7 @@ opcode_entry opcode_table [512] = {
 
 static make_EHelper(2byte_esc) {
   uint32_t opcode = instr_fetch(eip, 1) | 0x100;
+  printf("2-byte Opcode is: 0x%02x\n", opcode);
   decoding.opcode = opcode;
   set_width(opcode_table[opcode].width);
   idex(eip, &opcode_table[opcode]);
