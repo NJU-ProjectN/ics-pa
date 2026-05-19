90e9768a68b30d2e4956d9ae04927da29f1bda58
tracer-ics2017 <tracer@njuics.org>
2026-05-19 20:31:56 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:56 up  4:07,  3 users,  load average: 0.01, 0.01, 0.00 efb10afb49a08478e4be31d4e3e63229218f70ab

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -16,7 +16,17 @@ uint32_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  // 🟢 借用完美的 pmem_rw 宏进行安全的边界检查与直写！
+  // 根据不同的长度，强转成对应类型的指针并直接赋值
+  if (len == 1) {
+    pmem_rw(addr, uint8_t) = (uint8_t)data;
+  } else if (len == 2) {
+    pmem_rw(addr, uint16_t) = (uint16_t)data;
+  } else if (len == 4) {
+    pmem_rw(addr, uint32_t) = (uint32_t)data;
+  } else {
+    Assert(0, "Invalid internal write length: %d", len);
+  }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
