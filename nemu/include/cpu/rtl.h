042700675d5b2475f99b80a36ddc8ccd61688ef6
tracer-ics2017 <tracer@njuics.org>
2026-05-19 17:09:05 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:05 up 48 min,  2 users,  load average: 0.00, 0.01, 0.00 984cc9def64011484ad852245e772ec54f19d932

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -132,7 +132,7 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1) {
     else if (#f[0] == 'O') shift = 11; \
     *dest = (cpu.eflags >> shift) & 1; \
   }
-  
+
 make_rtl_setget_eflags(CF)
 make_rtl_setget_eflags(OF)
 make_rtl_setget_eflags(ZF)
@@ -140,17 +140,23 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  TODO();
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  *dest = ~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  if (width == 4) {
+    *dest = *src1;
+  } else if (width == 1) {
+    *dest = (int32_t)(int8_t)(*src1);
+  } else if (width == 2) {
+    *dest = (int32_t)(int16_t)(*src1);
+  }
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
@@ -171,22 +177,22 @@ static inline void rtl_pop(rtlreg_t* dest) {
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  *dest = (*src1 == 0) ? 1 : 0;
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  *dest = (*src1 == imm) ? 1 : 0;
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+  *dest = (*src1 != 0) ? 1 : 0;
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  *dest = (*src1 >> (width * 8 - 1)) & 1;
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
