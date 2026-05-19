6b1a2839231471c76df298cd52e90474605b5720
tracer-ics2017 <tracer@njuics.org>
2026-05-19 17:53:26 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:26 up  1:32,  2 users,  load average: 0.00, 0.04, 0.00 4dec9f1b488b0d34a59e17719c337355f5d0b6be

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -9,6 +9,8 @@ make_EHelper(sub);
 make_EHelper(xor);
 make_EHelper(cmp);
 make_EHelper(lea);
+make_EHelper(add);
+make_EHelper(and);
 
 typedef struct {
   DHelper decode;
@@ -50,8 +52,8 @@ static make_EHelper(name) { \
 
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
-    EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EX(sub), EX(xor), EX(cmp))
+    EX(add), EMPTY, EMPTY, EMPTY,
+    EX(and), EX(sub), EX(xor), EX(cmp))
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
