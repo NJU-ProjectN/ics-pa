3624b40b8421588f76dfe72a24edbaf36424588a
tracer-ics2017 <tracer@njuics.org>
2026-05-20 09:17:47 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:17:47 up  1:11,  3 users,  load average: 0.08, 0.06, 0.02 ae65c6c02872a7a8707b232c2f7e57c9f8205fbd

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -1,6 +1,7 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov) {
+  printf("MOV EIP: 0x%08x, ModRM: %02x, Disp: %d\n", cpu.eip, id_dest->type, id_dest->simm);
   rtl_li(&t0, id_src->val);
   operand_write(id_dest, &t0);
   
