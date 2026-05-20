070391a3969fd605711e1dec5f87e5730aa25265
tracer-ics2017 <tracer@njuics.org>
2026-05-20 20:48:30 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:48:30 up  3:54,  2 users,  load average: 0.00, 0.02, 0.00 fef7b4f14c4e714f6f12a709ad3ca31f8aead95b

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -10,7 +10,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_S, CC_NS, CC_P,  CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
-  rtlreg_t t0 = 0, t1 = 0;
+
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
