ba7799d9e29754b4112fe088199cae6046958b05
tracer-ics2017 <tracer@njuics.org>
2026-05-20 19:42:52 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:42:52 up  2:49,  2 users,  load average: 0.00, 0.03, 0.01 d918ae77b31edfb95497121cc7c73b978d8dc140

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -34,8 +34,11 @@ make_EHelper(call) {
     // push 返回地址
     printf("DEBUG: Call: pushing ret addr 0x%08x\n", decoding.seq_eip);
     int32_t offset = (int32_t)id_dest->val;
+    uint32_t target = decoding.seq_eip + offset;
     // seq_eip 应该是 call 指令的下一条指令地址 (0x10000f)
     // 目标地址应该是 0x10000f + offset = 0x10001c
+    uint32_t content = vaddr_read(target, 4);
+    printf("DEBUG: CALL target=0x%08x, content_at_target=0x%08x\n", target, content);
     printf("DEBUG: CALL calc: seq_eip=0x%08x, offset=0x%08x, target=0x%08x\n", 
             decoding.seq_eip, offset, decoding.seq_eip + offset);
     rtlreg_t ret_addr = decoding.seq_eip; 
