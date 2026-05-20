6a95988fb6d066d51e14a232d7ed015efb0d76b2
tracer-ics2017 <tracer@njuics.org>
2026-05-20 10:16:45 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:16:45 up  1:55,  3 users,  load average: 0.00, 0.00, 0.00 bc031040dfec6c2e2f7c131cf7a4b26c556ea71b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -246,16 +246,14 @@ static make_EHelper(2byte_esc) {
 
 make_EHelper(real) {
   vaddr_t temp_eip = *eip;
-  uint8_t first_byte = instr_fetch(eip, 1); 
-  *eip = temp_eip;
 
   uint32_t opcode = instr_fetch(eip, 1);
   decoding.opcode = opcode; 
   set_width(opcode_table[opcode].width);
   idex(eip, &opcode_table[opcode]);
 
-  printf("[LEN_CHECK] EIP: 0x%08x | Opcode: 0x%02x | Next_EIP: 0x%08x | Advanced: %d bytes\n", 
-       temp_eip, first_byte, *eip, (int)(*eip - temp_eip));
+printf("[LEN_CHECK] EIP: 0x%08x | Opcode: 0x%02x | Next_EIP: 0x%08x | Advanced: %d bytes\n", 
+       temp_eip, (uint8_t)opcode, *eip, (int)(*eip - temp_eip));
 }
 
 static inline void update_eip(void) {
