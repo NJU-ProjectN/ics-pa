ec3289804413dca241338e8358a4e07710f0245e
tracer-ics2017 <tracer@njuics.org>
2026-05-20 11:08:43 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:08:43 up  2:37,  3 users,  load average: 0.02, 0.03, 0.00 a8d5eefa9ed27cfd8e9f1f027dfc922ce16aef1e

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -162,7 +162,7 @@ opcode_entry opcode_table [512] = {
   /* 0xdc */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe4 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xe8 */	IDEXW(J, call, 4), IDEXW(J, jmp, 4), EMPTY, EMPTY,
+  /* 0xe8 */	IDEXW(J, call, 4), IDEXW(J, jmp, 4), EMPTY, IDEXW(J, jmp, 1),
   /* 0xec */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf4 */	EMPTY, EMPTY, IDEXW(E, gp3, 1), IDEX(E, gp3),
