ecc143c6aefe481c2fa8799fac96488cb4fed863
tracer-ics2017 <tracer@njuics.org>
2026-05-20 08:55:07 +0800

    >  compile 161220000 kujoenzo Linux kujoenzo-VMware-Virtual-Platform 6.17.0-23-generic #23~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 14 16:11:48 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:55:07 up 48 min,  3 users,  load average: 0.07, 0.03, 0.00 fee54986aa08b82d9c94db0287e961af915dc123

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -249,7 +249,11 @@ make_EHelper(real) {
 }
 
 static inline void update_eip(void) {
-  cpu.eip = (decoding.is_jmp ? (decoding.is_jmp = 0, decoding.jmp_eip) : decoding.seq_eip);
+  if (decoding.is_jmp) {
+    cpu.eip = decoding.jmp_eip;
+  } else {
+    cpu.eip = decoding.seq_eip;
+  }
 }
 
 void exec_wrapper(bool print_flag) {
@@ -276,14 +280,14 @@ void exec_wrapper(bool print_flag) {
 #ifdef DIFF_TEST
   uint32_t eip = cpu.eip;
 #endif
-
-  update_eip();
-  #ifdef DEBUG
+#ifdef DEBUG
   printf("[TRACE] EIP = 0x%08x, ESP = 0x%08x, ASM: %s\n", 
           decoding.is_jmp ? decoding.jmp_eip : decoding.seq_eip, cpu.esp, decoding.assembly);
   #else
   printf("[TRACE] EIP = 0x%08x, ESP = 0x%08x\n", cpu.eip, cpu.esp);
   #endif
+  update_eip();
+  
 #ifdef DIFF_TEST
   void difftest_step(uint32_t);
   difftest_step(eip);
